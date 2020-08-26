975126ba4ee5037374d128f8d9adb261c4558598
alexischup <alexischup@gmail.com>
2020-08-26 12:43:41 +0200

    Add sending feature

--- contactMail.php
+++ contactMail.php
@@ -1,10 +1,11 @@
 <?php
-    // $toEmail = "alexischup@gmail.com";
-    // $mailHeaders = "From: " . $_POST["username-name"] . "<". $_POST["username-email"] .">\r\n";
-    // if(mail($toEmail, "MESSAGE PROVENANT DU SITE", $_POST["username-message"], $mailHeaders)) {
-    //     print "<p class='success'>Mail Sent.</p>";
-    // } else {
-    //     print "<p class='Error'>Problem in Sending Mail.</p>";
-    // }
-    print_r('YOLO');
+    $toEmail = "alexischup@gmail.com";
+    $subject = "subject";
+    // $message = $_POST["username-message"];
+    $headers = 'From: devale.info@gmail.com';
+    if(mail($toEmail, $subject, "Message", $headers)) {
+        echo "<p class='success'>Mail Sent.</p>";
+    } else {
+        echo "<p class='Error'>Problem in Sending Mail.</p>";
+    }
 ?>
